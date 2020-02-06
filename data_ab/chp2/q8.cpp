6b93d446129339921ec62b2c796c2ec2b866c485
chuanhao01 <chuanhaolim@gmail.com>
2020-02-06 16:37:31 +0800

    Q8 is done

--- data_ab/chp2/q8.cpp
+++ data_ab/chp2/q8.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 
 // Functions
-int removeZeroElements(int scores[], int nScores){
-    for(int i=nScores - 1; i>-1; i--){
+int removeZeroElements(int scores[], int n_scores){
+    for(int i=n_scores - 1; i>-1; i--){
         if(scores[i] == 0){
-            for(int j=i; j<nScores - 1; j++){
-                // For current i until nScores-1
-                // Replace current with next, until nScores - 2 and nScores - 1
+            for(int j=i; j<n_scores - 1; j++){
+                // For current i until n_scores-1
+                // Replace current with next, until n_scores - 2 and n_scores - 1
                 scores[j] = scores[j+1];
             }
-            nScores--;
+            n_scores--;
         }
     }
-    return nScores;
+    return n_scores;
 }
 
 int main(){
-    int nScores = 13;
+    int n_scores = 13;
     int arr[20] = {65, 0, 95, 0, 0, 79, 82, 0, 84, 94, 86, 90, 0};
-    nScores = removeZeroElements(arr, nScores);
-    std::cout << "nSCores:" << nScores << std::endl;
-    for(int i=0; i<nScores; i++){
+    n_scores = removeZeroElements(arr, n_scores);
+    std::cout << "nSCores:" << n_scores << std::endl;
+    for(int i=0; i<n_scores; i++){
         std::cout<< arr[i] << ", ";
     }
     std::cout << std::endl;
