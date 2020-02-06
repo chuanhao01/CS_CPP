6f372e244421d567fd55c0f633e77254a6d9239e
chuanhao01 <chuanhaolim@gmail.com>
2020-02-06 17:03:31 +0800

    Starting on CS106B

---
+++ data_ab/chp2/q10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int getUserInput(int *user_inputs, int *n_user_inputs){
+    // Getting user input
+    int user_input;
+    std::cout << "? ";
+    std::cin >> user_input;
+    std::cout << '\n';
+    // Checking exit condition
+
+}
+
+int main(){
+
+    return 1;
+}
